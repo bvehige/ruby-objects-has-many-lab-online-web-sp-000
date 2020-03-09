37ae8d9031b567e6653a073b3c1fa7037f5257a5
Brian Vehige <brianvehige@gmail.com>
2020-03-09 08:35:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
 
 attr_accessor :name  
 
+@@post_count = 0
+
 def initialize(name)
   @name = name 
 end
@@ -11,15 +13,14 @@ def posts
 end
 
 def add_post(post)
-  post.author = self 
+  post.author = self
+  @@post_count += 1 
 end
 
 def add_post_by_title(title)
   post = Posts.new(title)
   add_post(post)
+  @@post_count += 1 
 end
 
-
-
-  
 end
