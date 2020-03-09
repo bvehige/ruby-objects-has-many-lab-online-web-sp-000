b1dff58e4e6d197cd379622a0d88492031fddf54
Brian Vehige <brianvehige@gmail.com>
2020-03-09 08:06:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,44 +1,32 @@
 class Artist 
 
+attr_accessor :name, :songs
 
+  @@song_count = 0
 
-attr_accessor :name
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-@@song_count = 0 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-def initialize(name)
-  @name = name 
-  @songs = []
-  
-end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-def add_song(song)
-  @songs << song
-  song.artist = self
-  @@song_count += 1 
-end
-
-def add_song_by_name(song_name)
-  song = Song.new(song_name)
-  song.artist = self 
-  @songs << song
-  @@song_count += 1 
-    
-end
-
-def songs
-  @songs
-end
-
-
-def self.song_count 
- @@song_count    
- end
- 
-
+  def self.song_count
+    @@song_count
+  end
 
 end
-
 
 
 
